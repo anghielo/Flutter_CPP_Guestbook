b806168aedefe4d163eac51d6b24fb2b69843dda
anghielo <user_noreply@github.com>
2021-10-28 08:56:42 -0700

    Update

---
+++ lib/components/message_bubble.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String sender;
+  final String text;
+  final bool isMe;
+
+  const MessageBubble({Key key, this.sender, this.text, this.isMe})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        // crossAxisAlignment is used to move messages to the right side
+        //crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            sender,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.black54,
+            ),
+          ),
+          Material(
+            // borderRadius: BorderRadius.circular(30.0),
+            // borderRadius: BorderRadius.only(
+            //   topLeft: Radius.circular(30.0),
+            //   bottomLeft: Radius.circular(30.0),
+            //   bottomRight: Radius.circular(30.0),
+            // ),
+            borderRadius: isMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  )
+                : BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  ),
+            // Elevation adds drop shadow to the messages
+            elevation: 5.0,
+            // color: Colors.blue,
+            color: isMe ? Colors.blue : Colors.teal,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                // '$text from $sender',
+                text,
+                style: TextStyle(
+                  fontSize: 15.0,
+                  // color: Colors.white,
+                  color: isMe ? Colors.white : Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
