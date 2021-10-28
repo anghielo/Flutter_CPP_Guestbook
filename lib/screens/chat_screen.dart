da725837e94e4d09f43f3e1c7ea2d9285f7395a2
anghielo <user_noreply@github.com>
2021-10-28 08:55:50 -0700

    Update

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:calpoly_tick_talk/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:calpoly_tick_talk/components/messages_stream.dart';
+
+final _firestore = Firestore.instance;
+FirebaseUser loggedInUser;
+
+class ChatScreen extends StatefulWidget {
+  // static String is used to call the page from the routes property of main
+  static String id = 'chat_screen';
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  // messageTextController will be used to clear the message after "Send" is pressed
+  final messageTextController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+
+  // messageText for messages that will be saved in Firestore
+  String messageText;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.email + ' logged in');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // void getMessages() async {
+  //   final messages = await _firestore.collection('messages').getDocuments();
+  //   for (var message in messages.documents) {
+  //     print(message.data);
+  //   }
+  // }
+
+  // Method will listen for stream of messages coming from Firestore
+  // Method messagesStream is replaced with widget MessagesStream()
+  void messagesStream() async {
+    // This is how you subscribe to the Firestore collection. Firestore pushes to stream
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.documents) {
+        print(message.data);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                messagesStream(); // Can be deleted after testing
+                // getMessages(); // Testing if data is pulling
+                // Logout user
+                _auth.signOut();
+                Navigator.pop(context);
+              }),
+        ],
+        centerTitle: true,
+        title: Text('What\'s on your mind?'),
+        backgroundColor: Color(0xFF16402D),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // MessageStream calls StreamBuilder to be able to update the chat from Firestore
+            MessagesStream(),
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      // messageTextController will control the message text field
+                      controller: messageTextController,
+                      onChanged: (value) {
+                        // The message text value becomes messageText.
+                        messageText = value;
+                      },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Send messages and clear the message text field
+                      messageTextController.clear();
+                      // messages is the collection in Firestore Database
+                      _firestore.collection('messages').add({
+                        'text': messageText,
+                        'sender': loggedInUser.email,
+                      });
+                    },
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:calpoly_tick_talk/constants.dart';
+import 'package:calpoly_tick_talk/components/rounded_button.dart';
+import 'package:calpoly_tick_talk/screens/chat_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+class LoginScreen extends StatefulWidget {
+  // static String is used to call the page from the routes property of main
+  static const String id = 'login_screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+  bool showSpinner = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              // Flexible widget can be flexible to allow compatibility between devices
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'myname@cpp.edu',
+                  labelText: 'Enter your email',
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: '******',
+                  labelText: 'Enter your password',
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              RoundedButton(
+                buttonTitle: 'Log In',
+                color: Color(0xFFF9AF0F),
+                onPress: () async {
+                  setState(() {
+                    // Indicate loading pending authentication
+                    showSpinner = true;
+                  });
+                  // print('Testing login');
+                  // print(email);
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (user != null) {
+                      // After successful login, usher user to the chat page
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                    setState(() {
+                      // Indicate loading complete
+                      showSpinner = false;
+                    });
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
